61f7ea068b341ba73ae89bbc4289016d48751cd6
41039138 <113663778+GUJIAFENG@users.noreply.github.com>
2024-10-14 11:20:41 +0000

    00_1

--- pages/00_Member.py
+++ pages/00_Member.py
@@ -15,13 +15,24 @@ for i in range(0, len(members), 2):
     for j in range(2):
         if i + j < len(members):
             with cols[j]:
-                st.write(f"""
-                <span style="font-size:20px;">{members[i + j]['name']}</span><br>
+                st.markdown(f"""
+                <div style="text-align: center;">
+                    <span style="font-size:20px;">{members[i + j]['name']}</span><br>
+                </div>
                 """, unsafe_allow_html=True)
                 if 'motto' in members[i + j]:
-                    st.write(f"""
-                    <span style="color:red; font-size:25px;">座右銘:</span><span style="font-size:20px;">{members[i + j]['motto']}</span>
+                    st.markdown(f"""
+                    <div style="text-align: center;">
+                        <span style="color:red; font-size:23px;">座右銘:</span><br>
+                        <span style="font-size:20px;">{members[i + j]['motto']}</span>
+                    </div>
                     """, unsafe_allow_html=True)
                 st.image(members[i + j]['image'], width=300)
     st.write("")  # 添加空行
 
+# 新增右對齊的內容
+st.markdown("""
+<div style="text-align: right;">
+    這是右對齊的內容。
+</div>
+""", unsafe_allow_html=True)
