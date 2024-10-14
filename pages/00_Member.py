41410d226b83eb79aded298e5dae88146cc8252d
41039138 <113663778+GUJIAFENG@users.noreply.github.com>
2024-10-14 11:00:34 +0000

    update

---
+++ pages/00_Member.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("小組成員")
+
+st.write("""
+<span style="font-size:20px;">hello 我是第七組川哥。</span>
+<br>
+<span style="color:red; font-size:25px;">座右銘:</span><span style="font-size:20px;">在尿都憋不住的年紀卻憋住了想你。</span>
+""", unsafe_allow_html=True)
+
+
+
+st.image("./img/82123.jpg", width=300)
--- template.py
+++ template.py
@@ -8,12 +8,3 @@ st.write("歡迎來到熱流力實驗報告。請從左側選單選擇要查看�
 
 # 不需要在這裡定義實驗項目列表，因為Streamlit會自動生成側邊欄
 
-st.write("""
-<span style="font-size:20px;">hello 我是第七組川哥。</span>
-<br>
-<span style="color:red; font-size:25px;">座右銘:</span><span style="font-size:20px;">在尿都憋不住的年紀卻憋住了想你。</span>
-""", unsafe_allow_html=True)
-
-
-
-st.image("./img/82123.jpg", width=300)
