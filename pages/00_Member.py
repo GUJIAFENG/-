b836e7387f0098578bc4557638f56e7d61ff039a
41039138 <113663778+GUJIAFENG@users.noreply.github.com>
2024-10-14 12:15:15 +0000

    00_1

--- pages/00_Member.py
+++ pages/00_Member.py
@@ -8,22 +8,22 @@ def example():
         falling_speed=7,
         animation_length="infinite",
     )
+    st.markdown("<br><br>", unsafe_allow_html=True)
+    st.title("小組成員")
 
-example()  
-st.markdown("<br><br>", unsafe_allow_html=True)
-st.title("小組成員")
+    # 第一位成員
+    st.write("""
+    <span style="font-size:20px;">機械設計系 41023130 林奇川 </span><br>
+    <span style="color:red; font-size:23px;">座右銘:</span><span style="font-size:20px;">在尿都憋不住的年紀卻憋住了想你。</span>
+    """, unsafe_allow_html=True)
+    st.image("./img/1.jpg", width=350)
 
-# 第一位成員
-st.write("""
-<span style="font-size:20px;">機械設計系 41023130 林奇川 </span><br>
-<span style="color:red; font-size:23px;">座右銘:</span><span style="font-size:20px;">在尿都憋不住的年紀卻憋住了想你。</span>
-""", unsafe_allow_html=True)
-st.image("./img/1.jpg", width=350)
+    st.markdown("<br><br><br>", unsafe_allow_html=True)
 
-st.markdown("<br><br><br>", unsafe_allow_html=True)
+    # 第二位成員
+    st.write("""
+    <span style="font-size:20px;">機械設計系 41023118 吳政憲</span><br>
+    """, unsafe_allow_html=True)
+    st.image("./img/2.jpg", width=350)
 
-# 第二位成員
-st.write("""
-<span style="font-size:20px;">機械設計系 41023118 吳政憲</span><br>
-""", unsafe_allow_html=True)
-st.image("./img/2.jpg", width=350)
+example()  # 調用函數展示動畫效果和內容
