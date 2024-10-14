f90d239b678a110060cbc7bcce46e3220beab279
41039138 <113663778+GUJIAFENG@users.noreply.github.com>
2024-10-14 11:25:10 +0000

    00_1

--- pages/00_Member.py
+++ pages/00_Member.py
@@ -2,37 +2,17 @@ import streamlit as st
 
 st.title("小組成員")
 
-# 定義成員信息
-members = [
-    {"name": "我是林奇川 可以叫我川哥。", "motto": "在尿都憋不住的年紀卻憋住了想你。", "image": "./img/82123.jpg"},
-    {"name": "我是陳濬祺 可以叫我地精。", "image": "./img/1.png"}
-    
-]
-
-# 佈局每行兩個成員
-for i in range(0, len(members), 2):
-    cols = st.columns(2)
-    for j in range(2):
-        if i + j < len(members):
-            with cols[j]:
-                st.markdown(f"""
-                <div style="text-align: center;">
-                    <span style="font-size:20px;">{members[i + j]['name']}</span><br>
-                </div>
-                """, unsafe_allow_html=True)
-                if 'motto' in members[i + j]:
-                    st.markdown(f"""
-                    <div style="text-align: center;">
-                        <span style="color:red; font-size:23px;">座右銘:</span><br>
-                        <span style="font-size:20px;">{members[i + j]['motto']}</span>
-                    </div>
-                    """, unsafe_allow_html=True)
-                st.image(members[i + j]['image'], width=300)
-    st.write("")  # 添加空行
-
-# 新增右對齊的內容
-st.markdown("""
-<div style="text-align: right;">
-    這是右對齊的內容。
-</div>
+# 第一位成員
+st.write("""
+<span style="font-size:20px;">我是41023130林奇川 可以叫我川哥。</span><br>
+<span style="color:red; font-size:23px;">座右銘:</span><span style="font-size:20px;">在尿都憋不住的年紀卻憋住了想你。</span>
 """, unsafe_allow_html=True)
+st.image("./img/82123.jpg", width=300)
+
+st.write("")  # 添加空白行來增加間距
+
+# 第二位成員
+st.write("""
+<span style="font-size:20px;">我是陳濬祺 可以叫我地精。</span><br>
+""", unsafe_allow_html=True)
+st.image("./img/1.png", width=300)
