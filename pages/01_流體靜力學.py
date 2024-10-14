6925f8eb14579ce77ad4d70984e53cd6bcb4403a
41039138 <113663778+GUJIAFENG@users.noreply.github.com>
2024-10-14 06:45:57 +0000

    更新01

--- pages/01_流體靜力學.py
+++ pages/01_流體靜力學.py
@@ -2,8 +2,14 @@ import streamlit as st
 
 st.title("實驗一: 流體靜力學")
 
-st.write("""<span style="font-size:25px;">hello 我是第七組川哥。</span>', unsafe_allow_html=True
-'<span style="color:red; font-size:30px;">座右銘:</span>', unsafe_allow_html=True在尿都憋不住的年紀卻憋住了想你。""")
+st.write("""
+<span style="font-size:25px;">hello 我是第七組川哥。</span>
+<br>
+<span style="color:red; font-size:30px;">座右銘:</span>
+<br>
+在尿都憋不住的年紀卻憋住了想你。
+""", unsafe_allow_html=True)
+
 
 # 在這裡添加實驗一的具體內容，如圖表、數據等
 st.image("./img/82123.jpg", width=500)
