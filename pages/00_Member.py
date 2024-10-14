0441c26e985204ff7237d761654595d45d5383be
41039138 <113663778+GUJIAFENG@users.noreply.github.com>
2024-10-14 11:41:36 +0000

    00_1

--- pages/00_Member.py
+++ pages/00_Member.py
@@ -4,15 +4,15 @@ st.title("小組成員")
 
 # 第一位成員
 st.write("""
-<span style="font-size:20px;">我是41023130林奇川 可以叫我川哥。</span><br>
+<span style="font-size:20px;">機械設計系 41023130 林奇川 </span><br>
 <span style="color:red; font-size:23px;">座右銘:</span><span style="font-size:20px;">在尿都憋不住的年紀卻憋住了想你。</span>
 """, unsafe_allow_html=True)
-st.image("./img/82123.jpg", width=300)
+st.image("./img/1.jpg", width=350)
 
 st.markdown("<br><br><br>", unsafe_allow_html=True)
 
 # 第二位成員
 st.write("""
-<span style="font-size:20px;">我是陳濬祺 可以叫我地精。</span><br>
+<span style="font-size:20px;">機械設計系 41023118 吳政憲</span><br>
 """, unsafe_allow_html=True)
-st.image("./img/1.png", width=300)
+st.image("./img/2.png", width=350)
