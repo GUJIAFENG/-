4a06180fbc74fd364315ad79f5c108fb2ae92957
41039138 <113663778+GUJIAFENG@users.noreply.github.com>
2024-11-04 15:36:09 +0000

    實驗2

--- pages/01_流體靜力學.py
+++ pages/01_流體靜力學.py
@@ -1,8 +1,47 @@
 import streamlit as st
 
-st.title("實驗一: 流體靜力學")
+st.markdown(""" <h1 style='text-align: center;'>實驗一: 實驗目的</h1> """, unsafe_allow_html=True)
 
-st.write("這裡是流體靜力學實驗的詳細內容。")
+st.markdown("<br><br><br>", unsafe_allow_html=True)
+
+st.write("""
+<div style='text-align: center;'>
+    <span style='font-size:30px;'>第七組實驗報告</span><br/>
+</div>
+""", unsafe_allow_html=True)
+
+st.markdown("<br>", unsafe_allow_html=True)
+
+st.write("""
+<span style='color:blue; font-size:25px;'>壹、 實驗目的</span><br/>
+以墨汁流入透明之壓克力管流中，觀察流體在管路中流動的情形，並配合計算出 Re(雷諾數) ，以膫解層流和紊流與雷諾數(Re)之間的關係。
+""", unsafe_allow_html=True)
+
+st.write("""
+<span style='color:blue; font-size:25px;'>貳、 儀器與設備</span><br/>
+水循環泵、驅動馬達、儲水槽、實驗台架、柏登壓力錶、流量控制閥、水衝擊台一套。
+""", unsafe_allow_html=True)
+
+st.image("./img/1104.png")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # 在這裡添加實驗一的具體內容，如圖表、數據等
