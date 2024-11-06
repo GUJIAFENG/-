a325a9b8ea98b8064c6c3a35ae87e0d6c1e5a71b
GUJIAFENG <41039138@gm.nfu.edu.tw>
2024-11-06 16:25:48 +0800

    report

--- pages/00_Member.py
+++ pages/00_Member.py
@@ -5,17 +5,11 @@ st.title("小組成員")
 
 # 第一位成員
 st.write("""
-<<<<<<< HEAD
-<span style="font-size:20px;">機械設計系 41023130 林奇川 我是傻逼 </span><br>
-<span style="color:red; font-size:23px;">座右銘:</span><span style="font-size:20px;">在尿都憋不住的年紀卻憋住了想你。</span>
-""", unsafe_allow_html=True)
-st.image("./img/1.jpg", width=350)
-=======
 <span style="font-size:20px;">機械設計系 41023130 林奇川 </span><br>
 <span style="color:red; font-size:23px;">座右銘:</span><span style="font-size:20px;">在尿都憋不住的年紀卻憋住了想你。</span>
 """, unsafe_allow_html=True)
 st.image("./img/00/1.jpg", width=350)
->>>>>>> 198dd46 (提交本地更改)
+
 
 st.markdown("<br><br><br>", unsafe_allow_html=True)
 
@@ -23,10 +17,7 @@ st.markdown("<br><br><br>", unsafe_allow_html=True)
 st.write("""
 <span style="font-size:20px;">機械設計系 41023118 吳政憲</span><br>
 """, unsafe_allow_html=True)
-<<<<<<< HEAD
-st.image("./img/2.jpg", width=350)
-=======
->>>>>>> 198dd46 (提交本地更改)
+
 
 st.markdown("<br><br>", unsafe_allow_html=True)
 
