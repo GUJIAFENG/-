a15dd3903c21f59e13323962cc7628ec0c54b82b
GUJIAFENG <41039138@gm.nfu.edu.tw>
2024-11-07 15:26:56 +0800

    01

--- pages/01_流體靜力學.py
+++ pages/01_流體靜力學.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 
-st.markdown(""" <h1 style='text-align: center;'>實驗一: 實驗目的</h1> """, unsafe_allow_html=True)
+st.markdown(""" <h1 style='text-align: center;'>實驗一: 雷諾數實驗</h1> """, unsafe_allow_html=True)
 
 st.markdown("<br><br><br>", unsafe_allow_html=True)
 
@@ -31,8 +31,8 @@ st.markdown("<br>", unsafe_allow_html=True)
 
 st.write("""
 <span style='color:blue; font-size:25px;'>參、 實驗原理</span><br/>
-研究流體力學的過程中，我們會遇到為數不少的無因次參數，如Re、Fr、Ma等， 但其中最為大家所熟知的則為Re，及雷諾數。
-其物理意義維慣性力與黏性力之比值，式中<span style='color:red;'>ρ、V、L、μ</span><br/>分別為密度、平均速度、特徵長度、絕對黏度(或動力黏度)，對一管流而言，特徵長度為直徑<span style='color:red; '>D</span><br/>
+研究流體力學的過程中，我們會遇到為數不少的無因次參數,如Re、Fr、Ma等, 但其中最為大家所熟知的則為Re,及雷諾數。
+其物理意義維慣性力與黏性力之比值，式中<span style='color:red;'>ρ、V、L、μ</span>分別為密度、平均速度、特徵長度、絕對黏度(或動力黏度)，對一管流而言，特徵長度為直徑<span style='color:red; '>D</span><br/>
 
 """, unsafe_allow_html=True)
 
@@ -42,9 +42,18 @@ st.markdown("<br>", unsafe_allow_html=True)
 
 st.image("./img/01/01-2.png")
 
+st.markdown("<br>", unsafe_allow_html=True)
+
 st.write("""
 <span style='color:blue; font-size:25px;'>肆、實驗步驟</span><br/>
-
+1. 墨水加水稀釋(約1:5)後裝入點滴液瓶內並裝置在儀器上端。
+2. 打開進水口閥及內管出水口閥，並將進出口流量控制在穩定流動狀 態(即外管水位維持在某一固定位置不變)。 
+3. 將墨水之控制閥打開讓墨水穩定的滴入套筒中。 
+4. 觀察墨水於管路中流動的情形(層流、紊流或於臨界區域)同時用 量杯(或水筒)量取流量並用碼錶確實測量時間(秒)將此等資料數據(流動情形、流量、測量時間)詳細計錄。 
+5. 改變流量(由小到大)至少取五種不同的流量，以確實觀察由層流變化到完全紊流的情形。
+6. 實驗結束，將墨水關閉，且洗淨針頭後置淸水桶內，以免墨汁乾化，堵塞針孔，同時開大進水閥(出口閥維持略開)讓淸水充滿套筒內 部(此時會有多餘的水從溢水口流出)讓其自然循環數分鐘將墨汁 淸洗掉。 
+7. 最後再將進水閥關閉，並打開出口閥和洩水閥將水排乾。 
+8. 擦淨儀器本身及四周地板。 
 """, unsafe_allow_html=True)
 
 st.markdown("<br>", unsafe_allow_html=True)
@@ -54,7 +63,12 @@ st.write("""
 
 """, unsafe_allow_html=True)
 
-
+st.write("""
+<span style='color:red;'>1. 依實驗之觀察和計算結果，試判斷層流和紊流的臨界區域値在何種範圍。 </span><br/>
+<span style='color:red;'>2. 爲何在靠近管路之進出口端點處，流動均呈不穩定現象。 </span><br/>
+<span style='color:red;'>3. 試繪出層流和紊流之流動情形？並說明層流和紊流時水分子的流動情形。 </span><br/>
+<span style='color:red;'>4. 依據實驗數據及觀察結果，本實驗和一般衆多書籍所敍述之數據是否符合？若不符合，你認爲原因出在那裡，應如何改善。 </span><br/>
+""", unsafe_allow_html=True)
 
 
 # 在這裡添加實驗一的具體內容，如圖表、數據等
