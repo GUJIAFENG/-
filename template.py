0ab71ad57df36a93f8f33f0f84972aa19d51d8d4
GUJIAFENG <41039138@gm.nfu.edu.tw>
2024-11-09 00:29:39 +0800

    test

--- template.py
+++ template.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-st.set_page_config(page_title="奶茶照片", layout="wide")
+st.set_page_config(page_title="熱流力實驗", layout="wide")
 
-st.title("王沛沛的奶茶")
+st.title("熱流力實驗第七組")
 
-st.write("歡迎來到奶茶的生活照。")
+st.write("歡迎來到熱流力實驗報告。請從左側選單選擇要查看的實驗項目。")
 
 # 不需要在這裡定義實驗項目列表，因為Streamlit會自動生成側邊欄
+
