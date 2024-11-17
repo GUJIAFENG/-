2efea53eea931840342001d7d67dc97400889f7b
GUJIAFENG <41039138@gm.nfu.edu.tw>
2024-11-17 22:54:01 +0800

    03

--- pages/03_沛沛豬.py
+++ pages/03_沛沛豬.py
@@ -1,8 +1,33 @@
 import streamlit as st
 
-# 本地視頻文件的路徑
-video_file = 'C:/milktea/img/03/03-1.mp4'  # 請替換為您的視頻文件路徑
+st.markdown(""" <h1 style='text-align: center;'>王沛沛影片區</h1> """, unsafe_allow_html=True)
 
-# 播放本地視頻
-st.video(video_file)
-st.video(video_file, start_time=0, format="video/mp4", width=640, height=480)
+st.markdown("<br><br><br>", unsafe_allow_html=True)
+
+st.write("""
+<div style='text-align: center;'>
+    <span style='font-size:30px;'>沛沛豬的自言自語</span><br/>
+</div>
+""", unsafe_allow_html=True)
+
+
+st.markdown("<br>", unsafe_allow_html=True)
+
+
+
+
+
+
+# 使用 GitHub 的 "raw" URL
+video_url = "https://raw.githubusercontent.com/GUJIAFENG/Streamlit-hw/live/img/03/03-1.mp4"
+
+# 使用 HTML 嵌入視頻，並調整大小
+video_html = f"""
+    <video width="300" height="400" controls>
+        <source src="{video_url}" type="video/mp4">
+        您的瀏覽器不支持播放此視頻。
+    </video>
+    """
+
+# 使用 st.markdown 嵌入 HTML
+st.markdown(video_html, unsafe_allow_html=True)
