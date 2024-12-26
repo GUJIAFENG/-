a6122cc6cdffd09591d002ee4ae19405efbe709e
GUJIAFENG <41039138@gm.nfu.edu.tw>
2024-12-26 16:03:37 +0800

    07

---
+++ pages/07_期末報告.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.markdown(""" <h1 style='text-align: center;'>實驗六: </h1> """, unsafe_allow_html=True)
+
+st.markdown("<br><br><br>", unsafe_allow_html=True)
+
+st.write("""
+<div style='text-align: center;'>
+    <span style='font-size:30px;'>第七組期末報告</span><br/>
+</div>
+""", unsafe_allow_html=True)
+
+st.markdown("<br>", unsafe_allow_html=True)
